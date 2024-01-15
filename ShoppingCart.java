6d2c7eed195421d6f5e0fc8139adbaa46c725574
Eman <152151707+owing-evident@users.noreply.github.com>
2024-01-15 13:34:39 +0600

    Create ShoppingCart.java

---
+++ ShoppingCart.java
@@ -0,0 +1,19 @@
+// ShoppingCart.java
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List<Product> cartItems;
+
+    public ShoppingCart() {
+        this.cartItems = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        cartItems.add(product);
+    }
+
+    public List<Product> getCartItems() {
+        return cartItems;
+    }
+}
